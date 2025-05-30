b725322dead7007dde201edaa1b2bb63fb1a9977
ForceClose31 <forceclose31@gmail.com>
2025-05-30 15:45:18 +0700

    fetch API for unit jasa page

--- resources/views/comprof/unit-jasa.blade.php
+++ resources/views/comprof/unit-jasa.blade.php
@@ -5,36 +5,23 @@
 @section('content')
     <div id="profile" class="min-h-screen font-sans flex flex-col justify-center flex-wrap">
 
-        <section class="flex flex-col w-full justify-center pt-14 pb-3 text-center px-4 relative">
+        <section class="flex flex-col w-full justify-center pt-14 pb-3 text-center px-4 relative" x-data>
             <div class="w-full md:w-3/4 m-auto flex flex-col gap-4 dark:bg-gray-900 bg-white p-8 pb-4 rounded-lg">
                 <h1 class="text-4xl font-bold text-orange-500 mb-2">JASA UMUM DAN PPOB</h1>
                 <div class="flex flex-col md:flex-row gap-8">
                     <img src="{{ asset('images/unit-jasa.png') }}" alt="Cooperative Officials"
                         class="w-full md:w-80 object-cover rounded-s-3xl rounded-e-3xl" />
-                    <ol class="list-decimal text-sm text-justify text-black dark:text-white mt-5 space-y-1">
-                        <li>Jasa Pembayaran PPOB (Listrik, PDAM, Telepon, BPJS, Pulsa, dll)</li>
-                        <li>Jasa Layanan Tiket Kereta Api</li>
-                        <li>Jasa Sewa Aula, Sewa Tempat Outdoor, dan Cafe</li>
-                        <li>Jasa Pengurusan STNK</li>
-                    </ol>
+                    <div class="text-sm text-justify dark:text-white text-black" x-html="$store.unitJasa.jasaUmum"></div>
                 </div>
             </div>
         </section>
 
-        <section class="flex flex-col justify-center pt-3 pb-10 text-center px-4 relative">
+        <section class="flex flex-col justify-center pt-3 pb-10 text-center px-4 relative" x-data>
             <div class="w-full md:w-3/4 m-auto flex flex-col gap-4 dark:bg-gray-900 bg-white p-8 rounded-lg">
                 <img src="{{ asset('images/unit-jasa-2.png') }}" alt="Cooperative Officials"
                     class="w-full mb-4 object-cover rounded-s-3xl rounded-e-3xl m-auto" />
                 <h1 class="text-4xl font-bold text-orange-500 mb-2">JASA RENTAL KENDARAAN DAN PUJASERA</h1>
-                <p class="text-sm text-justify text-black dark:text-white">
-                    Koperasi KPRI UNEJ juga menyediakan layanan sewa atau rental kendaraan untuk umum dan anggota (khusus
-                    anggota akan mendapatkan harga sewa khusus).
-                    Armada yang tersedia saat ini adalah 2 unit Toyota Hi-Ace (termasuk sopir).
-                    Selain layanan rental kendaraan, Koperasi KPRI UNEJ juga mempunyai tempat/lapak Pujasera yang disewakan
-                    untuk umum dan anggota.
-                    Untuk info sewa dan rental,
-                    <span class="font-bold">Hubungi: Sdr. Agus Praptomo, 0852-3682-7345</span>
-                </p>
+                <div class="text-sm text-justify dark:text-white text-black" x-html="$store.unitJasa.jasaRental"></div>
             </div>
         </section>
         <a href="https://wa.me/6281234567890" target="_blank"
@@ -44,3 +31,35 @@
         </a>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.store('unitJasa', {
+                jasaRental: '',
+                jasaUmum: '',
+            });
+
+        });
+
+        fetch("https://kpri.fasilkomapp.com/api/service-types/4")
+            .then(res => res.json())
+            .then(result => {
+                if (result.success && result.data && Array.isArray(result.data.layanan)) {
+                    result.data.layanan.forEach(item => {
+                        let html = item.deskripsi;
+
+                        html = html.replace(/<ol>/,
+                            '<ol class="list-decimal pl-6 space-y-2 text-justify">');
+
+                        if (item.judul.toLowerCase() === 'jasa umum dan ppob') {
+                            Alpine.store('unitJasa').jasaUmum = html;
+                        } else if (item.judul.toLowerCase() === 'jasa rental kendaraan dan pujasera') {
+                            Alpine.store('unitJasa').jasaRental = html;
+                        }
+                    });
+                }
+            })
+            .catch(err => console.error("Gagal mengambil data profil:", err));
+    </script>
+@endpush
