29eac12fe8753e5dffea0d0692d2880c181651b1
ForceClose31 <forceclose31@gmail.com>
2025-05-30 16:18:10 +0700

    fetch API for unit toko page

--- resources/views/comprof/unit-toko.blade.php
+++ resources/views/comprof/unit-toko.blade.php
@@ -5,38 +5,13 @@
 @section('content')
     <div id="profile" class="min-h-screen font-sans mb-14 justify-center flex flex-wrap">
 
-        <section class="flex flex-col justify-center pt-12 pb-3 text-center px-4 relative w-full">
+        <section class="flex flex-col justify-center pt-12 pb-3 text-center px-4 relative w-full" x-data>
             <img src="{{ asset('images/unit-toko.png') }}" alt="Cooperative Officials"
                 class="w-full md:w-3/4 mb-4 object-cover p-0 md:p-8 rounded-s-3xl rounded-e-3xl m-auto" />
             <div class="w-3/4 m-auto flex flex-col gap-4 bg-white dark:bg-gray-900 p-8 rounded-lg">
-
                 <h1 class="text-4xl font-bold text-orange-500 mb-2">PRODUK</h1>
-                <p class="text-sm text-justify text-black dark:text-white">
-                    Produk-produk yang disediakan oleh KP-RI Universitas Jember sangatlah beragam dan memliki harga yang
-                    bersaing,
-                    jenis produk yang dijual antara lain :
-                </p>
-
-                <p class="font-bold text-left">LANTAI 1 KP-RI UNEJ</p>
-                <ol class="list-decimal list-inside text-left">
-                    <li>Kebutuhan pokok (Konsumsi): Beras, gula, minyak, mie instan, susu bayi/dewasa, dll.</li>
-                    <li>Kebutuhan pokok (non-konsumsi): sabun mandi, sabun cuci, kosmetik, parfume, dll.</li>
-                    <li>Snack, softdrink dan makanan ringan</li>
-                </ol>
-
-                <p class="font-bold text-left mt-4">LANTAI 2 KP-RI UNEJ</p>
-                <ol class="list-decimal list-inside text-left">
-                    <li>Peralatan Elektronik: kipas angin, TV, Tape, Magic com, dll</li>
-                    <li>Alat-alat listrik: sekering, kabel, adaptor, dll</li>
-                    <li>Peralatan rumah tangga: keranjang, alat makan, alat kebersihan, dll</li>
-                    <li>Tekstil: pakaian, kerudung, sarung, tas, dll</li>
-                </ol>
-
-                <p class="font-bold text-left mt-4">PESANAN PRODUK KHUSUS</p>
-                <p class="text-left">
-                    Koperasi juga menyediakan produk yang tidak tersedia melalui rekanan seperti komputer, motor, rumah,
-                    dll.
-                </p>
+                <div class="text-sm text-justify dark:text-white text-black space-y-2"
+                    x-html="$store.unitToko.produk"></div>
 
                 <h1 class="text-4xl font-bold mt-6 text-orange-500 mb-2">PROMO</h1>
 
@@ -48,8 +23,10 @@
                                     <div class="border-2 border-orange-400 rounded-lg py-4 px-2 flex flex-col items-center">
                                         <img src="{{ asset('images/' . $item['img']) }}" alt="{{ $item['text'] }}"
                                             class="w-full md:w-3/4 rounded shadow-md" />
-                                        <h2 class="text-center font-bold text-green-800 dark:text-white mt-2">{{ $item['text'] }}</h2>
-                                        <h2 class="text-center font-bold text-black dark:text-white">{{ $item['harga'] }}</h2>
+                                        <h2 class="text-center font-bold text-green-800 dark:text-white mt-2">
+                                            {{ $item['text'] }}</h2>
+                                        <h2 class="text-center font-bold text-black dark:text-white">{{ $item['harga'] }}
+                                        </h2>
                                     </div>
                                 </li>
                             @endforeach
@@ -66,3 +43,31 @@
         </a>
     </div>
 @endsection
+@push('scripts')
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.store('unitToko', {
+                produk: '',
+            });
+
+        });
+
+        fetch("https://kpri.fasilkomapp.com/api/service-types/6")
+            .then(res => res.json())
+            .then(result => {
+                if (result.success && result.data && Array.isArray(result.data.layanan)) {
+                    result.data.layanan.forEach(item => {
+                        let html = item.deskripsi;
+
+                        html = html.replace(/<ol>/,
+                            '<ol class="list-decimal pl-6 space-y-2 text-justify">');
+
+                        if (item.judul.toLowerCase() === 'produk') {
+                            Alpine.store('unitToko').produk = html;
+                        }
+                    });
+                }
+            })
+            .catch(err => console.error("Gagal mengambil data profil:", err));
+    </script>
+@endpush
